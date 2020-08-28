83296274a9b6454d06020ddf9b1c9587e043afb8
Surdyhey <sodiq.ajayi@yahoo.com>
2020-08-28 20:05:14 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, abort, jsonify, request, render_template
+from sklearn.externals import joblib
+from joblib import load
+from feature import *
+import json
+from removal import *
+
+pipeline = joblib.load('pipeline.sav')
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/api',methods=['POST'])
+def get_delay():
+
+    result=request.form
+    query_title = result['title']
+    query_author = result['author']
+    query_text = result['maintext']
+    print(query_text)
+    query = get_all_query(query_title, query_author, query_text)
+
+    print(query)
+    print("\n\n\n")
+    query = punc_removal(query)
+    print("SODIQ-------------------")
+    print(query)
+
+    #query = remove_punctuation_stopwords_lemma(query)
+
+    user_input = {'query':query}
+    pred = pipeline.predict(query)
+    print(pred)
+    dic = {1:'real',0:'fake'}
+    return f'<html><body><h1>{dic[pred[0]]}</h1> <form action="/"> <button type="submit">back </button> </form></body></html>'
+
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
